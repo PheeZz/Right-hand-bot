f38760a66bf8769a989b8246429f55b8a605c77e
PheeZz <jagubkoff@gmail.com>
2022-09-30 15:54:32 +0200

    remove blur from ocr

--- utils/ocr.py
+++ utils/ocr.py
@@ -9,11 +9,9 @@ def ocr(image='data/temp_ocr.png'):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.threshold(gray, 0, 255,
                          cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    gray = cv2.medianBlur(gray, 3)
     filename = "{}.png".format(os.getpid())
     cv2.imwrite(f'data/{filename}', gray)
     text = pytesseract.image_to_string(
         Image.open(f'data/{filename}'), lang='rus')
     os.remove(f'data/{filename}')
     return text
-
